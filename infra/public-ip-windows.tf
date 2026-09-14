e28e633b359c01871ed6a4deb630ee2945eed01e
lvbeserra <lvbeserra@users.noreply.github.com>
2026-09-14 15:35:58 +0000

    Manage existing Windows public IPs with Terraform

---
+++ infra/public-ip-windows.tf
@@ -0,0 +1,37 @@
+locals {
+  windows_public_ips = {
+    client = {
+      name     = "CLIENT01-ip"
+      location = "eastus"
+      zones    = ["1"]
+    }
+    sccm = {
+      name     = "SCCM01-ip"
+      location = "eastus"
+      zones    = null
+    }
+    ad = {
+      name     = "DC01-ip"
+      location = "northcentralus"
+      zones    = null
+    }
+  }
+}
+
+resource "azurerm_public_ip" "windows" {
+  for_each = local.windows_public_ips
+
+  name                    = each.value.name
+  resource_group_name     = "RG-LAB-WIN2025"
+  location                = each.value.location
+  zones                   = each.value.zones
+  allocation_method       = "Static"
+  sku                     = "Standard"
+  sku_tier                = "Regional"
+  ip_version              = "IPv4"
+  idle_timeout_in_minutes = 4
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
