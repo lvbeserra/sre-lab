c89e646de81b92c06c7af43dc6510509aa6b5e34
lvbeserra <lvbeserra@users.noreply.github.com>
2026-09-14 15:38:43 +0000

    Manage existing CLIENT01 network interface with Terraform

---
+++ infra/nic-client.tf
@@ -0,0 +1,24 @@
+resource "azurerm_network_interface" "client" {
+  name                           = "client0114"
+  resource_group_name            = "RG-LAB-WIN2025"
+  location                       = "eastus"
+  accelerated_networking_enabled = true
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    primary                       = true
+    subnet_id                     = azurerm_subnet.client.id
+    private_ip_address_allocation = "Dynamic"
+    private_ip_address_version    = "IPv4"
+    public_ip_address_id          = azurerm_public_ip.windows["client"].id
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "client" {
+  network_interface_id      = azurerm_network_interface.client.id
+  network_security_group_id = azurerm_network_security_group.windows["client"].id
+}
