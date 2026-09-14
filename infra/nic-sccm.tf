91fdf8916619341cd04a159309de16d1d7a0c77f
lvbeserra <lvbeserra@users.noreply.github.com>
2026-09-14 15:40:55 +0000

    Manage existing SCCM01 network interface with Terraform

---
+++ infra/nic-sccm.tf
@@ -0,0 +1,24 @@
+resource "azurerm_network_interface" "sccm" {
+  name                = "sccm01646"
+  resource_group_name = "RG-LAB-WIN2025"
+  location            = "eastus"
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    primary                       = true
+    subnet_id                     = azurerm_subnet.client.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.0.4"
+    private_ip_address_version    = "IPv4"
+    public_ip_address_id          = azurerm_public_ip.windows["sccm"].id
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "sccm" {
+  network_interface_id      = azurerm_network_interface.sccm.id
+  network_security_group_id = azurerm_network_security_group.windows["sccm"].id
+}
